16719c75f756e7447b67a306571b1bae68db9663
Stefan Dimitrov <stefan@dimitrov.li>
2016-08-22 22:04:53 -0400

    Fixed routing.

--- api/controllers/IndexController.js
+++ api/controllers/IndexController.js
@@ -4,10 +4,11 @@
  * @description :: Server-side logic for managing indices
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
+var path = require('path');
 
 module.exports = {
   index: function(req, res) {
-    res.sendfile(__dirname, 'index.html');
+    res.sendfile(path.join(__dirname, '../../assets/', 'index.html'));
   }
 };
 
